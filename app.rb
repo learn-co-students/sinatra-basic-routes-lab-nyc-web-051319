58b3bf9c4916ab0f04f3c6891ecb49cdd8cef53b
Constantine Chutis <constantinec84@gmail.com>
2019-06-03 02:27:05 -0400

    Finished

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Constantine"
+    end
+
+    get '/hometown' do
+        "My hometown is Middle Island"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is No Idea it's like 2AM and I just wanna sleep."
+    end
 end
