18798510bed276794f24466fd9e7b248e686e14f
Elma Rahman <elmarahman635@gmail.com>
2019-06-03 00:26:48 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Elma"
+    #return resp.status = 200
+  end
+
+  get '/hometown' do
+    "My hometown is Woodside"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Hello"
+  end
+
 end
